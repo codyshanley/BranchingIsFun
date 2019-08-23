39c5da355f8b95e15749f2987d7c9120d0eb74da
cody <cody@codyshanley.com>
2019-08-22 20:33:15 -0400

    first commit

---
+++ BranchingTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BranchingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's do some branching.");
+        }
+    }
+}
